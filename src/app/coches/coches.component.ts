72d4b46ca13ad818d88a9d4f4bb36478865478b0
Fernando Amezcua Alcántar <fernando@d80.mx>
2017-06-19 18:55:36 -0500

    Haciendo Peticiones HTTP

--- src/app/coches/coches.component.ts
+++ src/app/coches/coches.component.ts
@@ -1,19 +1,41 @@
 import { Component } from '@angular/core';
 import { Coche } from './coche';
+import { PeticionesService } from '../services/peticiones.service'; 
 
 @Component({
     selector: 'coches',
-    templateUrl: './coches.component.html'
+    templateUrl: './coches.component.html',
+    providers: [ PeticionesService ]
 })
 
 export class CochesComponent{
-    public coche: Coche; 
-
-    constructor(){
+    public coche: Coche;
+    public coches:Array<Coche>;
+    public articulos;
+    
+    constructor(
+        private _peticionesService: PeticionesService
+    ){
         this.coche = new Coche("","","");
+        this.coches = [
+             new Coche("Nissan","123","blanco"),
+             new Coche("Toyota","1113","Azul")
+        ];
     }
 
+    ngOnInit(){
+        this._peticionesService.getArticulos().subscribe(
+            result => {
+                console.log(result);
+            },
+            error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+            }
+        );
+    }
     onSubmit(){
-        console.log(this.coche);
+        this.coches.push(this.coche);
+        this.coche = new Coche("","","");
     }
 }
